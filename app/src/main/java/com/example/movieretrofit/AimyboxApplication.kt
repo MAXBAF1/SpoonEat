16cf6663999e3dc314a95cdc4c0e32000459111b
lloppy <ankudinovapol@gmail.com>
2023-04-07 03:32:06 +0500

    create voice animation, in process: grams editor

---
+++ app/src/main/java/com/example/movieretrofit/AimyboxApplication.kt
@@ -0,0 +1,32 @@
+package com.example.movieretrofit
+
+import android.app.Application
+import android.content.Context
+import com.justai.aimybox.Aimybox
+import com.justai.aimybox.api.aimybox.AimyboxDialogApi
+import com.justai.aimybox.components.AimyboxProvider
+import com.justai.aimybox.core.Config
+import com.justai.aimybox.speechkit.google.platform.GooglePlatformSpeechToText
+import com.justai.aimybox.speechkit.google.platform.GooglePlatformTextToSpeech
+import java.util.*
+
+class AimyboxApplication : Application(), AimyboxProvider {
+
+    companion object {
+        private const val AIMYBOX_API_KEY = "Ldf0j7WZi3KwNah2aNeXVIACz0lb9qMH"
+    }
+
+    override val aimybox by lazy { createAimybox(this) }
+
+    private fun createAimybox(context: Context): Aimybox {
+        val unitId = UUID.randomUUID().toString()
+
+        val textToSpeech = GooglePlatformTextToSpeech(context, Locale.ENGLISH)
+        val speechToText = GooglePlatformSpeechToText(context, Locale.ENGLISH)
+
+        val dialogApi = AimyboxDialogApi(AIMYBOX_API_KEY, unitId)
+
+        val aimyboxConfig = Config.create(speechToText, textToSpeech, dialogApi)
+        return Aimybox(aimyboxConfig, context)
+    }
+}
---
+++ app/src/main/java/com/example/movieretrofit/interfaces/OnGramsReceivedListener.kt
@@ -0,0 +1,4 @@
+package com.example.movieretrofit.interfaces
+
+interface OnGramsReceivedListener {
+}
